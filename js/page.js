5247f0a56f044dff41cc14b9745813dd4edfe49f
Nariman Safiulin <woofilee@gmail.com>
2016-09-08 20:57:23 +0300

    Some edits - New year fixes - Math page deleted - Comment blocks in code removed

--- js/page.js
+++ js/page.js
@@ -1,36 +1,30 @@
 (function() {
   avatar = document.getElementById("avatar");
-  // coub = document.getElementById("coub");
-  name_div = document.getElementById("name");
-  info_div = document.getElementById("info");
   parallax = new Parallax(document.getElementById("scene"));
 
-  // avatar.addEventListener("click", function() {
-  //   coub.style.display = "inline-block";
-  //   this.style.display = "none";
-  //   name_div.style.display = "none";
-  //   info_div.style.display = "none";
-  // });
-
   media = document.getElementById("media");
-  voldown = document.getElementById("voldown");
-  volup = document.getElementById("volup");
-  copy_music = document.getElementById("music");
+  volume_down = document.getElementById("volume_down");
+  volume_up = document.getElementById("volume_up");
+  music_copy = document.getElementById("music_copy");
 
   media.addEventListener("play", function() {
-    copy_music.style.opacity = 1;
+    music_copy.style.opacity = 1;
   });
+
   media.addEventListener("pause", function() {
-    copy_music.style.opacity = 0;
+    music_copy.style.opacity = 0;
   });
+
   media.addEventListener("ended", function() {
-    copy_music.style.opacity = 0;
+    music_copy.style.opacity = 0;
     media.currentTime = 0;
   });
-  voldown.addEventListener("click", function() {
+
+  volume_down.addEventListener("click", function() {
     media.volume = Math.max(0, media.volume - 0.1);
   });
-  volup.addEventListener("click", function() {
+
+  volume_up.addEventListener("click", function() {
     media.volume = Math.min(1, media.volume + 0.1);
   });
 
